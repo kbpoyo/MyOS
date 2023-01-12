51a7c25cfcf88bc73f8e2c5b41a8a7f6e9e57b6e
kbpoyo <2420022198@qq.com>
2023-01-12 12:26:17 +0800

    add KERNEL_STACKA_SIZE

--- source/kernel/include/os_cfg.h
+++ source/kernel/include/os_cfg.h
@@ -11,8 +11,6 @@
 
 #ifndef OS_CFG_H
 #define OS_CFG_H
-
-
 /**
  * @brief   定义全局描述符表的大小，理论最大为8192
  *          全局描述符表用 lgdt （48位内存数据）来加载
@@ -23,9 +21,12 @@
 #define GDT_TABLE_SIZE 8192
 
 //内核代码段的选择子,gdt_table[1]
-#define KERNEL_SELECTOR_CS ((uint16_t)(0x0001 << 3))
+#define KERNEL_SELECTOR_CS (0x0001 << 3)
 
 //内核数据段的选择子,gdt_table[2]
-#define KERNEL_SELECTOR_DS ((uint16_t)(0x0002 << 3))
+#define KERNEL_SELECTOR_DS (0x0002 << 3)
+
+//内核栈空间的大小为8kb
+#define KERNEL_STACK_SIZE (8 * 1024)
 
 #endif
