197e1f6b55e43768a48189b52e342f8c6900cee2
kbpoyo <2420022198@qq.com>
2023-08-27 23:23:58 +0800

    kbos version 0.1

--- source/boot/boot.h
+++ source/boot/boot.h
@@ -1,11 +1,17 @@
 /**
- * 自己动手写操作系统
- *
- * 系统引导部分，启动时由硬件加载运行，然后完成对二级引导程序loader的加载
- * boot扇区容量较小，仅512字节。由于dbr占用了不少字节，导致其没有多少空间放代码，
- * 所以功能只能最简化,并且要开启最大的优化-os
- *
+ * @file boot_info.h
+ * @author kbpoyo (kbpoyo.com)
+ * @brief  系统引导部分，启动时由硬件加载运行，然后完成对二级引导程序loader的加载
+ *         boot扇区容量较小，仅512字节。由于dbr占用了不少字节，导致其没有多少空间放代码，
+ *         所以功能只能最简化,并且要开启最大的优化-os
+ * @versioinin0.1
+ * @date 2023-01-04
+ * 
+ * @copyright Copyright (c) 2023
+ * 
  */
+
+
 #ifndef BOOT_H
 #define BOOT_H
 
